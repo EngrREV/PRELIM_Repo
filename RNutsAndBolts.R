b0d41723dda18b59bf8c7abd996f1ffccf0ce51a
EngrREV <revirata@gmail.com>
2018-07-12 16:00:27 +0800

    Update

--- RNutsAndBolts.R
+++ RNutsAndBolts.R
@@ -70,4 +70,135 @@ unclass(x)
 
 x<- factor(c("Heaven","Hell","Heaven","Hell","Heaven","Heaven","Heaven"), levels = c("Hell","Heaven"))
 x
-           
+
+##Missing Values
+x<- c(1,2,NA,10,3)
+is.na(x)
+is.nan(x)
+
+x<- c(1,2,NaN,NA,4)
+is.na(x)
+is.nan(x)
+
+##DataFrames
+x<-data.frame(foo = 1:4, bar = c(T,T,F,F))
+nrow(x)
+ncol(x)
+
+##Names
+x<- 1:3
+names(x)
+names(x)<- c("foo","bar","norf")
+x
+names(x)
+
+##Names-List
+x<- list(a = 1,b = 2, c = 3)
+x
+##Names- Matrices
+m<- matrix(1:4, nrow =2, ncol = 2)
+dimnames(m) <- list(c("a","b"),c("c","d"))
+m
+
+##Ddput-ting R Objects
+y <- data.frame(a = 1 , b = "a")
+dput(y)
+dput(y, file = "y.R")
+y <- dget("y.R")
+y
+
+##Dumping
+x<- "foo"
+y<- data.frame(a = 1, b = "a")
+dump(c("x","y"), file = "data.R")
+rm(x,y)
+source("data.R")
+y
+x
+
+##Subsetting
+x<- c("a", "b", "c", "c", "d", "a")
+x[1]
+x[2]
+x[1:4]
+x[x > "a"]
+u<- x > "a"
+u
+x[u]
+
+##Subsetting-List
+x<-list(foo = 1:4, bar = 0.6)
+x[1]
+x[[1]]
+x$bar
+x[["bar"]]
+x["bar"]
+
+x<- list(foo = 1:4, bar = 0.6 , baz = "hello")
+x[c(1, 3)]
+
+x<- list(foo = 1:4, bar = 0.6 , baz = "hello")
+name<- "foo"
+x[[name]]
+x$name
+x$foo
+
+#Nested Elements of list
+x<-list(a = list(10,12,14), b = c(3.14,2.81))
+x[[c(1,3)]]
+x[[1]][[3]]
+
+x[[c(2,1)]]
+
+##Subsetting matrices
+x<-matrix(1:6,2,3)
+x[1,2]
+x[2,1]
+x[1, ]
+x[, 2]
+
+x<-matrix(1:6,2,3)
+x[1,2]
+
+x[1,2,drop = FALSE]
+
+x<-matrix(1:6,2,3)
+x[1, ]
+x[1, , drop = FALSE]
+
+##Subsetting with names(Partial Matching)
+x<-list(aardvark = 1:5)
+x$a
+x[["a"]]
+x[["a", exact = FALSE]]
+
+##Removing NA Values
+x<-c(1,2, NA, 4, NA, 5)
+bad <- is.na(x)
+x[!bad]
+
+x<-c(1,2, NA, 4, NA, 5)
+y<-c("a","b", NA, "d", NA, "f")
+good <- complete.cases(x,y)
+good
+x[good]
+y[good]
+
+airquality[1:6, ]
+good <- complete.cases(airquality)
+
+airquality[good, ][1:6, ]
+
+##Vectorized Operations
+x<- 1:4; y<- 6:9
+x + y
+x > 2
+x >= 2
+y == 8
+x * y
+x / y
+
+x<-matrix(1:4, 2, 2) ; y <- matrix(rep(10,4), 2, 2)
+x * y
+x / y
+x %*% y
